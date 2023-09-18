12101b64c867ed3356fbe53fed195ec54d535bae
matheus.arcelino <matheus.arcelino@mds.gov.br>
2023-09-18 11:03:37 -0300

    Importação do projeto base

---
+++ upload-file-api/src/main/java/br/com/arcelino/uploadfileapi/UploadFileApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.uploadfileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileApiApplication.class, args);
+	}
+
+}
