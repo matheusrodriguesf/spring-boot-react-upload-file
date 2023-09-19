fba2026e1f8746afe531b4bd77b28184b430c3de
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-09-19 09:12:16 -0300

    Add Campo Descrição

--- upload-files-web/src/components/UploadFile.tsx
+++ upload-files-web/src/components/UploadFile.tsx
@@ -1,11 +1,26 @@
-import { Box, Button, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from "@mui/material";
-import { Component, ChangeEvent } from "react";
+import React, { Component, ChangeEvent, KeyboardEvent } from "react";
+import {
+    Box,
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton,
+    TextField,
+} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
 type Props = {};
 
+type FileWithDescription = {
+    file: File;
+    description: string;
+};
+
 type State = {
-    selectedFiles: File[] | null;
+    selectedFiles: FileWithDescription[] | null;
 };
 
 class UploadFile extends Component<Props, State> {
@@ -19,8 +34,12 @@ class UploadFile extends Component<Props, State> {
     handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const selectedFiles = event.target.files;
         if (selectedFiles) {
+            const filesWithDescription = Array.from(selectedFiles).map((file) => ({
+                file,
+                description: "",
+            }));
             this.setState((prevState) => ({
-                selectedFiles: [...(prevState.selectedFiles || []), ...Array.from(selectedFiles)],
+                selectedFiles: [...(prevState.selectedFiles || []), ...filesWithDescription],
             }));
         }
     };
@@ -33,16 +52,48 @@ class UploadFile extends Component<Props, State> {
         event.preventDefault();
         const { files } = event.dataTransfer;
         if (files.length > 0) {
+            const filesWithDescription = Array.from(files).map((file) => ({
+                file,
+                description: "",
+            }));
             this.setState((prevState) => ({
-                selectedFiles: [...(prevState.selectedFiles || []), ...Array.from(files)],
+                selectedFiles: [...(prevState.selectedFiles || []), ...filesWithDescription],
             }));
         }
     };
 
-    removeFile = (file: File) => {
+    removeFile = (file: FileWithDescription) => {
         this.setState((prevState) => ({
-            selectedFiles: prevState.selectedFiles?.filter((f) => f !== file) || null,
+            selectedFiles:
+                prevState.selectedFiles?.filter((f) => f !== file) || null,
         }));
+    };
+
+    handleDescriptionChange = (
+        event: ChangeEvent<HTMLInputElement>,
+        fileWithDescription: FileWithDescription
+    ) => {
+        const { value } = event.target;
+        this.setState((prevState) => ({
+            selectedFiles: prevState.selectedFiles?.map((f) =>
+                f === fileWithDescription ? { ...f, description: value } : f
+            ) || null,
+        }));
+    };
+
+    handleDescriptionKeyUp = (
+        event: React.KeyboardEvent<HTMLInputElement>,
+        fileWithDescription: FileWithDescription
+    ) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            const { value } = event.currentTarget;
+            this.setState((prevState) => ({
+                selectedFiles: prevState.selectedFiles?.map((f) =>
+                    f === fileWithDescription ? { ...f, description: value } : f
+                ) || null,
+            }));
+        }
     };
 
     render() {
@@ -57,9 +108,14 @@ class UploadFile extends Component<Props, State> {
                 alignItems="center"
                 justifyContent="center"
                 onDragOver={this.handleDragOver}
-                onDrop={this.handleDrop}>
+                onDrop={this.handleDrop}
+            >
                 <label htmlFor="upload-image">
-                    <Button variant="outlined" component="div" startIcon={<CloudUploadIcon />}>
+                    <Button
+                        variant="outlined"
+                        component="div"
+                        startIcon={<CloudUploadIcon />}
+                    >
                         Anexar Arquivos
                     </Button>
                 </label>
@@ -73,14 +129,26 @@ class UploadFile extends Component<Props, State> {
                 />
                 {this.state.selectedFiles && this.state.selectedFiles.length > 0 && (
                     <List>
-                        {this.state.selectedFiles.map((file, index) => (
+                        {this.state.selectedFiles.map((fileWithDescription, index) => (
                             <ListItem key={index}>
-                                <ListItemText primary={file.name} />
+                                <ListItemText primary={fileWithDescription.file.name} />
+                                <TextField
+                                    label="Descrição"
+                                    variant="outlined"
+                                    size="small"
+                                    value={fileWithDescription.description}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                        this.handleDescriptionChange(event, fileWithDescription)
+                                    }
+                                    onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) =>
+                                        this.handleDescriptionKeyUp(event, fileWithDescription)
+                                    }
+                                />
                                 <ListItemSecondaryAction>
                                     <IconButton
                                         edge="end"
                                         aria-label="delete"
-                                        onClick={() => this.removeFile(file)}
+                                        onClick={() => this.removeFile(fileWithDescription)}
                                     >
                                         <DeleteIcon />
                                     </IconButton>
