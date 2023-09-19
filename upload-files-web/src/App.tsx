5e61346c3e9c8a51ce33dacd21bc2d7d31cf300a
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-09-19 08:28:33 -0300

    Inclusão dependencia mui material

--- upload-files-web/src/App.tsx
+++ upload-files-web/src/App.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 
 
 function App() {
   return (
     <div>
-      <h1>React Typescript</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
